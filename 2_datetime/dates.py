3ad99e2953621a465b8938976806b78de1296401
Gabriel Almir <avlye@outlook.com>
2020-07-26 11:32:40 -0300

    Add datetime basics

---
+++ 2_datetime/dates.py
@@ -0,0 +1,18 @@
+from datetime import date, time, datetime
+
+# Get today's date
+today = date.today()
+print("Today's date is", today)
+
+# Print out the date's individual components
+print("Date:", today.day, "/", today.month, "/", today.year)
+
+days = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+    "Sunday"
+]
+print("Today's weekday number is", today.weekday())
+print("Today is", days[today.weekday()])
+
+# Time now!
+print("Now:", datetime.now())
